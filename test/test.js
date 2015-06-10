8afebaf3229291caf0f43876f1357271d9a7335a
Marvel Mathew <marvel@verus.io>
2015-06-10 14:02:46 -0500

    Add failing test

--- test/test.js
+++ test/test.js
@@ -38,6 +38,9 @@ function run(seleniumParams) {
     }
 
     describe('Changing the quantity of a line item', function() {
+      it('should fail', function() {
+        assert.equals(true, false);
+      })
       it("should update line item cost", function(done) {
         var unitPrice = 0;
         function parsePrice(str) {
